71400a05b53d5e446d295a33f5193331501f48f9
reinzcity <reinz.developer@gmail.com>
2022-12-12 15:04:46 +0700

    modified install-boot.sh

--- install-boot.sh
+++ install-boot.sh
@@ -2,14 +2,13 @@
 
 if [[ "$UID" != 0 ]]; then
   echo "USER NOT ROOT"
-  sudo $0
+  sudo su
   exit
 else
   echo "USER IS ROOT"
 fi
 
-
-sudo pacman -S plymouth
+pacman -S plymouth
 
 cd good
 cp --recursive --force --remove-destination --no-dereference --preserve=links . /
